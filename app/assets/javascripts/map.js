b4e747d6e077888c624e751e2625a8fb5b4ac413
Kris Sparks <kbs5280@gmail.com>
2016-09-15 17:26:49 -0600

    Google places working with dynamic lat and long KEY removed
    Need to implement environment variable. Ready for user save-search feature.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,53 +1,56 @@
 //= require jquery
+$(document).ready(function() {
+  var map;
+  var infowindow;
 
-$(function() {
+  function initialize() {
+      console.log('map?')
+      var latitude = $("div[id='map']").attr("data-latitude"),
+          longitude = $("div[id='map']").attr("data-longitude");
 
-var map;
-var infowindow;
+      var center = new google.maps.LatLng(latitude, longitude);
+      map = new google.maps.Map(document.getElementById('map'), {
+          center: center,
+          zoom: 11
+      });
 
-function initialize() {
-    console.log('map?')
-    var latitude = $("div[id='map']").attr("data-latitude"),
-        longitude = $("div[id='map']").attr("data-longitude");
+      var request = {
+          location: center,
+          radius: 16000,
+          types: ['campground']
+        };
+      infowindow = new google.maps.InfoWindow();
 
-    var center = new google.maps.LatLng(latitude, longitude);
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: center,
-        zoom: 11
-    });
+      var service = new google.maps.places.PlacesService(map);
+      service.nearbySearch(request, callback);
+    }
 
-    var request = {
-        location: center,
-        radius: 16000,
-        types: ['campground']
-      };
-    infowindow = new google.maps.InfoWindow();
-
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
+    function callback(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+        }
+     }
   }
 
-  function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
-   }
-}
+    function createMarker(place) {
+      var placeLoc = place.geometry.location;
+      var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+      });
 
-  function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-      map: map,
-      position: place.geometry.location
-    });
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+      });
+  }
 
-    google.maps.event.addListener(marker, 'click', function() {
-      infowindow.setContent(place.name);
-      infowindow.open(map, this);
-    });
-}
+      google.maps.event.addDomListener(window, 'load', initialize)
+      console.log('i ran after');
 
-    google.maps.event.addDomListener(window, 'load', initialize)
+})
+$(function() {
+  console.log('i ran brefore');
 
 });
